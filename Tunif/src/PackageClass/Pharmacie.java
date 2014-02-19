2d31e9328f0f3bf5ae8e025a029d2b2c00a225a5
Med <Med@Amine>
2014-02-19 16:13:58 +0100

    modif

---
+++ Tunif/src/PackageClass/Pharmacie.java
@@ -0,0 +1,72 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PackageClass;
+
+/**
+ *
+ * @author Med
+ */
+public class Pharmacie {
+    
+        private int code_pharmacie;
+        private String nom_pharmacie;
+        private String adresse_pharmacie;
+        private int tel_pharmacie;
+        private String type_pharmacie;
+        
+        public Pharmacie(int code_pharmacie,String nom_pharmacie,String adresse_pharmacie,int tel_pharmacie,String type_pharmacie){
+        this.code_pharmacie=code_pharmacie;
+        this.nom_pharmacie=nom_pharmacie;
+        this.adresse_pharmacie=adresse_pharmacie;
+        this.tel_pharmacie=tel_pharmacie;
+        this.type_pharmacie=type_pharmacie;
+        }
+
+    public int getCode_pharmacie() {
+        return code_pharmacie;
+    }
+
+    public String getNom_pharmacie() {
+        return nom_pharmacie;
+    }
+
+    public String getAdresse_pharmacie() {
+        return adresse_pharmacie;
+    }
+
+    public int getTel_pharmacie() {
+        return tel_pharmacie;
+    }
+
+    public String getType_pharmacie() {
+        return type_pharmacie;
+    }
+
+    public void setCode_pharmacie(int code_pharmacie) {
+        this.code_pharmacie = code_pharmacie;
+    }
+
+    public void setNom_pharmacie(String nom_pharmacie) {
+        this.nom_pharmacie = nom_pharmacie;
+    }
+
+    public void setAdresse_pharmacie(String adresse_pharmacie) {
+        this.adresse_pharmacie = adresse_pharmacie;
+    }
+
+    public void setTel_pharmacie(int tel_pharmacie) {
+        this.tel_pharmacie = tel_pharmacie;
+    }
+
+    public void setType_pharmacie(String type_pharmacie) {
+        this.type_pharmacie = type_pharmacie;
+    }
+
+    @Override
+    public String toString() {
+        return "Pharmacie{" + "code_pharmacie=" + code_pharmacie + ", nom_pharmacie=" + nom_pharmacie + ", adresse_pharmacie=" + adresse_pharmacie + ", tel_pharmacie=" + tel_pharmacie + ", type_pharmacie=" + type_pharmacie + '}';
+    }
+        
+}
---
+++ Tunif/src/PackageClass/Pharmacien.java
@@ -0,0 +1,83 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PackageClass;
+
+/**
+ *
+ * @author Med
+ */
+public class Pharmacien {
+    private int id_pharm;
+    private String nom_pharm;
+    private String pre_pharm;
+    private String email_pharm;
+    private String login_pharm;
+    private String mdp_pharm;
+
+    public Pharmacien(int id_pharm, String nom_pharm, String pre_pharm, String email_pharm, String login_pharm, String mdp_pharm) {
+        this.id_pharm = id_pharm;
+        this.nom_pharm = nom_pharm;
+        this.pre_pharm = pre_pharm;
+        this.email_pharm = email_pharm;
+        this.login_pharm = login_pharm;
+        this.mdp_pharm = mdp_pharm;
+    }
+
+    public int getId_pharm() {
+        return id_pharm;
+    }
+
+    public String getNom_pharm() {
+        return nom_pharm;
+    }
+
+    public String getPre_pharm() {
+        return pre_pharm;
+    }
+
+    public String getEmail_pharm() {
+        return email_pharm;
+    }
+
+    public String getLogin_pharm() {
+        return login_pharm;
+    }
+
+    public String getMdp_pharm() {
+        return mdp_pharm;
+    }
+
+    public void setId_pharm(int id_pharm) {
+        this.id_pharm = id_pharm;
+    }
+
+    public void setNom_pharm(String nom_pharm) {
+        this.nom_pharm = nom_pharm;
+    }
+
+    public void setPre_pharm(String pre_pharm) {
+        this.pre_pharm = pre_pharm;
+    }
+
+    public void setEmail_pharm(String email_pharm) {
+        this.email_pharm = email_pharm;
+    }
+
+    public void setLogin_pharm(String login_pharm) {
+        this.login_pharm = login_pharm;
+    }
+
+    public void setMdp_pharm(String mdp_pharm) {
+        this.mdp_pharm = mdp_pharm;
+    }
+
+    @Override
+    public String toString() {
+        return "Pharmacien{" + "id_pharm=" + id_pharm + ", nom_pharm=" + nom_pharm + ", pre_pharm=" + pre_pharm + ", email_pharm=" + email_pharm + ", login_pharm=" + login_pharm + ", mdp_pharm=" + mdp_pharm + '}';
+    }
+    
+    
+    
+}
