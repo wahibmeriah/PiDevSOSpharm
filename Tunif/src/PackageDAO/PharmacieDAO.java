409bd43255cf77fcec1485aaa346771cc557d74c
Meriah <Meriah@192.168.1.42>
2014-02-19 17:11:15 +0000

    3ém

--- Tunif/src/PackageDAO/PharmacieDAO.java
+++ Tunif/src/PackageDAO/PharmacieDAO.java
@@ -31,6 +31,23 @@ public class PharmacieDAO {
            //Logger.getLogger(PersonneDao.class.getName()).log(Level.SEVERE, null, ex);
             System.out.println("erreur lors de l'insertion "+ex.getMessage());
         }
+     }
         //
+          public void updatePharmacie(Pharmacie d){
+        String requete = "update pharmacie set code_pharmacie=?, set nom_pharmacie=?, set adresse_pharmacie=?, set tel_pharmacie=?,set type_pharmacie=? where code_pharmacie=?";
+        try {
+            PreparedStatement ps = Connection.getInstance().prepareStatement(requete);
+            ps.setInt(1, d.getCode_pharmacie());
+            ps.setString(2, d.getNom_pharmacie());
+            ps.setString(3, d.getAdresse_pharmacie());
+            ps.setInt(4, d.getTel_pharmacie());
+             ps.setString(5, d.getType_pharmacie());
+            ps.executeUpdate();
+            System.out.println("Mise à jour effectuée avec succès");
+        } catch (SQLException ex) {
+           //Logger.getLogger(PersonneDao.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("erreur lors de la mise à jour "+ex.getMessage());
+        }
     }
+    
 }
